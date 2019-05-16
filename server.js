b45562566ee4c9c006d55b45e8224f1d003bdfaf
NabeelMustafa <nabeelmustafa729@gmail.com>
2019-05-16 15:24:01 +0500

    basic routes for all --testing

--- server.js
+++ server.js
@@ -7,4 +7,11 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 app.get('/', (req, res) => res.send('Api running'));
+
+// define routes
+app.use('/api/users', require('./routes/api/user'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profiles', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/post'));
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
